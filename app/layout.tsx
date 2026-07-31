d9639f93f1826168d1a966a52da830b671b0b6ae
abhishekdutta619 <abhishekdutta619@gmail.com>
2026-07-31 10:10:50 +0530

    fix: suppress body hydration warning caused by browser extensions

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${display.variable} ${body.variable} ${mono.variable}`}>
-      <body>{children}</body>
+      <body suppressHydrationWarning>{children}</body>
     </html>
   );
 }
